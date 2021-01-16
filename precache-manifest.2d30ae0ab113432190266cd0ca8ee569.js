34b17c3e8f50123d956c81c350b394996caff967
Galuh Ramadhan <galuh.html@gmail.com>
2021-01-17 06:49:01 +0700

    Updates

---
+++ precache-manifest.2d30ae0ab113432190266cd0ca8ee569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36101c2a5bf91b4bf3bc4cc1d46665f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "37c97032066e8990d2a2",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "d635cc736a715f1fa598",
+    "url": "./static/js/2.35dbffc5.chunk.js"
+  },
+  {
+    "revision": "df34426e3c67cba54df30dae4655a86f",
+    "url": "./static/js/2.35dbffc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c97032066e8990d2a2",
+    "url": "./static/js/main.925432ec.chunk.js"
+  },
+  {
+    "revision": "ccc2f0e11cb905b5c80f",
+    "url": "./static/js/runtime-main.f052381b.js"
+  }
+]);
